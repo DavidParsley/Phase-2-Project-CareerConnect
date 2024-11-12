f0463f4c3a2f08d6c80ab1f8bef267492fc41cc3
Anne Muriuki <muriukianne05@gmail.com>
2024-11-12 13:01:31 +0300

    adds hover to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,18 +11,18 @@ export default function Navbar() {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <a className="nav-link" aria-current="page" href="#"><strong className='xyz'>Home</strong></a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">About</a>
+          <a className="nav-link" href="#"><strong className='xyz'>About</strong></a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Add Job</a>
+          <a className="nav-link" href="#"><strong className='xyz'>Add Job</strong></a>
         </li>
       </ul>
       <form className="d-flex">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
+        <button className="btn btn-outline-secondary" type="submit">Search</button>
       </form>
     </div>
   </div>
