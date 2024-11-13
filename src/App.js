b21e4a1a4920505f2dde25e7c68ff9ae0f55cbb9
David Parsley <davidparsley.kakhayanga@gmail.com>
2024-11-14 01:25:33 +0300

    adds sort jobs feature

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import About from "./pages/About";
 import NoPage from "./pages/NoPage";
 import Job from "./pages/Job";
 import AddJob from "./pages/AddJob";
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useEffect, useState } from "react";
 
 function App() {
   const [jobs, setJobs] = useState([]);
+  const [search, setSearch] = useState("");
+  const [onlyAvailableJobs, setOnlyAvailableJobs] = useState(false); 
 
   useEffect(() => {
     fetch("http://localhost:3000/jobs")
@@ -21,18 +23,18 @@ function App() {
       });
   }, []);
 
-  const [search, setSearch] = useState("");
-  const filter = jobs.filter((job) =>
+  
+  const filteredJobs = jobs.filter((job) =>
     job.title.toLowerCase().includes(search.toLowerCase())
   );
+
+  const availableJobs = onlyAvailableJobs ? filteredJobs.filter((job) => job.available) : filteredJobs;
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={<Layout search={search} setSearch={setSearch} />}>
-          <Route index element={<Home jobs={filter} />} />
+        <Route path="/" element={<Layout search={search} setSearch={setSearch}   setOnlyAvailableJobs={setOnlyAvailableJobs} />}>
+          <Route index element={<Home jobs={availableJobs} />}/>
           <Route path="/about" element={<About />} />
           <Route path="/job/:id" element={<Job />} />
           <Route path="addJob" element={<AddJob />} />
